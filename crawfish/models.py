1fc9ea15b4b2b3520fdb68a580cc4a0db6e10b34
HeathLee <753940818@qq.com>
2016-06-30 18:39:12 +0800

    daily commit

--- crawfish/models.py
+++ crawfish/models.py
@@ -2,6 +2,7 @@
 
 import os
 from django.db import models
+from users.models import User
 
 
 class Word(models.Model):
@@ -15,7 +16,7 @@ class Word(models.Model):
     # 学习度
     target_retention = models.IntegerField(default=5)
     # 单词内容
-    content = models.CharField(max_length=30, null=False)
+    content = models.CharField(max_length=30, null=False, unique=True)
     # 音标
     pronunciation = models.CharField(max_length=30)
     # 发音url
@@ -24,11 +25,36 @@ class Word(models.Model):
     ctime = models.DateTimeField(auto_now_add=True)
     # 修改时间
     mtime = models.DateTimeField(auto_now=True)
+    remark = models.TextField()
 
 
-# class User(models.Model):
-#     id = models.AutoField(primary_key=True)
-#     email = models.EmailField()
-#     passwd = models.CharField(max_length=32)
+class MyUser(User):
+    cet4_offset = models.IntegerField()
+    cet6_offset = models.IntegerField()
+    ielts_offset = models.IntegerField()
+    toefl_offset = models.IntegerField()
+    word_limit = models.IntegerField(max_length=4, default=20)
+    remark = models.TextField()
 
 
+class Level(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20, null=False, unique=True)
+    ctime = models.DateTimeField(auto_now_add=True)
+    mtime = models.DateTimeField(auto_now=True)
+    remark = models.TextField()
+
+
+class LevelWord(models.Model):
+    id = models.AutoField(primary_key=True)
+    level_id = models.IntegerField()
+    word_id = models.IntegerField()
+
+
+class Note(models.Model):
+    id = models.AutoField(primary_key=True)
+    word_id = models.IntegerField()
+    content = models.TextField()
+    ctime = models.DateTimeField(auto_now_add=True)
+    mtime = models.DateTimeField(auto_now=True)
+    remark = models.TextField()
